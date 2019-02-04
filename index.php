a2ecaa2ff5588eda3fea97f491a69c5ed7dc34cb
Tiago Baldo <tiago.baldo@br.experian.com>
2019-02-04 14:48:22 -0200

    heroku deploy config

---
+++ index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Poliana Psicologia - Psicologia Clínica</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar" data-aos="fade-down" data-aos-delay="500">
+      <div class="container">
+        <a class="navbar-brand" href="index.html"><span class="flaticon-psi"></span> Poliana Psicologia</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.html" class="nav-link">Início</a></li>
+            <li class="nav-item"><a href="perfil.html" class="nav-link">Perfil</a></li>
+            <li class="nav-item"><a href="especialidades.html" class="nav-link">Especialidades</a></li>
+            <li class="nav-item"><a href="psicoterapia-pra-que.html" class="nav-link">Psicoterapia para quê?</a></li>
+            <li class="nav-item"><a href="contato.html" class="nav-link">Agendar consulta</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- END nav -->
+
+    <section class="ftco-cover overlay" style="background-image: url(images/bg_1.jpg);" id="section-home" data-aos="fade"  data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center ftco-vh-100">
+          <div class="col-md-8 text-center">
+            <h1 class="ftco-heading mb-4" data-aos="fade-up" data-aos-delay="500"><span class="flaticon-quotation position-quote"></span>Definir objetivos é o primeiro passo para tornar o invisível em algo visível.</h1>
+            <h2 class="h5 ftco-subheading mb-5" data-aos="fade-up"  data-aos-delay="600" style="float: right;">Tony Robbins</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+    <div class="space">
+
+    </div>
+    <!-- <div class="ftco-section ftco-counter" id="section-counter">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap" data-aos="fade-up">
+            <div class="block-18 d-flex align-items-center">
+              <div class="icon mr-4">
+                <span class="flaticon-lawyer"></span>
+              </div>
+              <div class="text">
+                <strong class="number" data-number="1000">0</strong>
+                <span>Qualified Lawyer</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap" data-aos="fade-up">
+            <div class="block-18 d-flex align-items-center">
+              <div class="icon mr-4">
+                <span class="flaticon-handshake"></span>
+              </div>
+              <div class="text">
+                <strong class="number" data-number="12000">0</strong>
+                <span>Trusted Clients</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap" data-aos="fade-up">
+            <div class="block-18 d-flex align-items-center">
+              <div class="icon mr-4">
+                <span class="flaticon-medal"></span>
+              </div>
+              <div class="text">
+                <strong class="number" data-number="10000">0</strong>
+                <span>Won Cases</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap" data-aos="fade-up">
+            <div class="block-18 d-flex align-items-center">
+              <div class="icon mr-4">
+                <span class="flaticon-trophy"></span>
+              </div>
+              <div class="text">
+                <strong class="number" data-number="12921">5143</strong>
+                <span>Honor &amp; Awards</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+    <section class="ftco-section-2">
+      <div class="container-fluid">
+        <div class="section-2-blocks-wrapper row no-gutters">
+          <div class="img col-sm-12 col-md-6" style="background-image: url('images/image_4.jpg');" data-aos="fade-right"></div>
+          <div class="text col-md-6">
+            <div class="text-inner align-self-start" data-aos="fade-up">              
+              <h3>Formada em psicologia pela Universidade Paulista</h3>
+              <p>Realizo atendimento psicológico particular, atendendo público adulto, idoso e adolescente.</p>
+            </div>
+          </div>
+        </div>        
+      </div>
+    </section>
+
+    <div class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-5">
+          <div class="col-md-7 text-center" data-aos="fade-up">
+            <h2>O que fazemos</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 col-lg-4" data-aos="fade-up">
+            <div class="media block-6 d-block text-center">
+              <div class="icon mb-4"><span class="flaticon-gavel"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Psicoterapia</h3>
+                <p>A psicoterapia consiste em um conjunto de técnicas utilizadas para que o paciente entre em contato com seu inconsciente.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="media block-6 d-block text-center">
+              <div class="icon mb-4"><span class="flaticon-balance"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Atendimento infantil</h3>
+                <p>Tratar traumas de da infância</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="media block-6 d-block text-center">
+              <div class="icon mb-4"><span class="flaticon-lawyer"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Psicologia para enfermos</h3>
+                <p>Uma enfermidade pode causar problemas, vamos resolver.</p>
+              </div>
+            </div>    
+          </div>
+
+          <!-- <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="media block-6 d-block text-center">
+              <div class="icon mb-4"><span class="flaticon-courthouse"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Civil Ligitation</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="400">
+            <div class="media block-6 d-block text-center">
+              <div class="icon mb-4"><span class="flaticon-handshake"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Business Law</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="500">
+            <div class="media block-6 d-block text-center">
+              <div class="icon mb-4"><span class="flaticon-health-insurance"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Insurance Defense</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>    
+          </div> -->
+
+        </div>
+      </div>
+    </div>
+    
+
+  <div class="ftco-section testimony-img" id="ftco-testimony" style="background-image: url(images/image_8.jpg);" data-aos="fade"  data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row justify-content-center mb-5 pb-5">
+        <div class="col-md-7 text-center heading" data-aos="fade-up">
+          <h2>Depoimentos</h2>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row d-flex no-gutters">
+        <div class="col-lg align-self-sm-end" data-aos="fade-up">
+          <div class="testimony">
+             <blockquote>
+                <p>&ldquo;Seja o primeiro a dar um depoimento&rdquo;</p>
+              </blockquote>
+              <div class="author d-flex mt-4">
+                <div class="image mr-3 align-self-center">
+                  <!-- <img src="images/person_1.jpg" alt=""> -->
+                </div>
+                <!-- <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div> -->
+              </div>
+          </div>
+        </div>
+        <!-- <div class="col-lg align-self-sm-end" data-aos="fade-up">
+          <div class="testimony overlay">
+             <blockquote>
+                <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.&rdquo;</p>
+              </blockquote>
+              <div class="author d-flex mt-4">
+                <div class="image mr-3 align-self-center">
+                  <img src="images/person_2.jpg" alt="">
+                </div>
+                <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+              </div>
+          </div>
+        </div>
+        <div class="col-lg align-self-sm-end" data-aos="fade-up">
+          <div class="testimony">
+             <blockquote>
+                <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small  line of blind text by the name. &rdquo;</p>
+              </blockquote>
+              <div class="author d-flex mt-4">
+                <div class="image mr-3 align-self-center">
+                  <img src="images/person_3.jpg" alt="">
+                </div>
+                <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+              </div>
+          </div>
+        </div>
+        <div class="col-lg align-self-sm-end" data-aos="fade-up">
+          <div class="testimony overlay">
+             <blockquote>
+                <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however.&rdquo;</p>
+              </blockquote>
+              <div class="author d-flex mt-4">
+                <div class="image mr-3 align-self-center">
+                  <img src="images/person_2.jpg" alt="">
+                </div>
+                <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+              </div>
+          </div>
+        </div>
+        <div class="col-lg align-self-sm-end" data-aos="fade-up">
+          <div class="testimony">
+            <blockquote>
+              <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small  line of blind text by the name. &rdquo;</p>
+            </blockquote>
+            <div class="author d-flex mt-4">
+              <div class="image mr-3 align-self-center">
+                <img src="images/person_3.jpg" alt="">
+              </div>
+              <div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </div>  
+
+   <div class="ftco-section">
+    <div class="container">
+      <div class="row justify-content-center mb-5 pb-5">
+        <div class="col-md-7 text-center"  data-aos="fade-up">
+          <h2>Valores</h2>
+          <p>Por questões éticas e jurídicas os valores, não podem ser divulgados, entre em contato para detalhes</p>
+        </div>
+      </div>
+      <div class="row block-9" data-aos="fade-up">
+        <div class="col-md-6 pr-md-5">
+          <form action="#">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Seu nome">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Seu e-mail">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Assunto">
+            </div>
+            <div class="form-group">
+              <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Mensagem"></textarea>
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Enviar" class="btn btn-primary">
+            </div>
+          </form>
+        </div>
+        <div class="col-md-6" data-aos="fade-up" id="map"></div>
+      </div>
+    </div>
+  </div>
+  
+  <footer class="ftco-footer ftco-bg-dark ftco-section">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Sobre</h2>
+            <ul class="list-unstyled">
+              <li><a href="index.html" class="py-2 d-block">Início</a></li>
+              <li><a href="perfil.html" class="py-2 d-block">Perfil</a></li>
+              <li><a href="especialidades.html" class="py-2 d-block">Especialidades</a></li>
+              <li><a href="psicoterapia-pra-que.html" class="py-2 d-block">Psicoterapia para quê?</a></li>
+              <li><a href="contato.html" class="py-2 d-block">Agendar Consulta</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md">
+           <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Informações de contato</h2>
+            <ul class="list-unstyled">
+              <li><a href="javascript:return" class="py-2 d-block">Rua um, numero 10</a></li>
+              <li><a href="javascript:return" class="py-2 d-block">11 9 8888-2222</a></li>
+              <li><a href="javascript:return" class="py-2 d-block">contato@poliana.com</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md">
+           <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Horario de atendimento</h2>
+            <ul class="list-unstyled">
+              <li><a href="javascript:return" class="py-2 d-block">Clínica: Segunda - Sexta: 9:00 - 18:00</a></li>
+              <li><a href="javascript:return" class="py-2 d-block">Plantões Online: Todos os dias das 8:00 as 20:00</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 text-center">
+
+          <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5LI2fO9ngCU5E8CNeBzsnPWsc_gh02Ok"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
